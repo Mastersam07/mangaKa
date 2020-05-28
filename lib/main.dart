a95ab5dc28e2ff587e474a06bb4421df0e0eb8b1
Mastersam <abadasamuelosp@gmail.com>
2020-05-28 01:43:24 -0700

    update to views

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:komikkurida/models/data.dart';
+import 'package:komikkurida/views/detail.dart';
+import 'package:komikkurida/views/home.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return new MaterialApp(
+      title: 'GeekLib',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primarySwatch: Colors.yellow,
+        platform: TargetPlatform.iOS,
       ),
-      home: MyHomePage(title: 'komikkurida'),
+      home: Home(),
+      onGenerateRoute: (settings) => generateRoute(settings),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  ///Generate parameterized route --> e.g: detail/some book title
+  generateRoute(RouteSettings settings) {
+    final path = settings.name.split('/');
+    final title = path[1];
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    Book book = books.firstWhere((it) => it.title == title);
+    return MaterialPageRoute(
+      settings: settings,
+      builder: (context) => Detail(book),
     );
   }
 }
