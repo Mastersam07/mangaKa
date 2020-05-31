4072091058f8ac83e063b76c859a7c9bee2469b6
Mastersam <abadasamuelosp@gmail.com>
2020-05-31 03:17:20 -0700

    [changes] refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:komikkurida/models/data.dart';
-import 'package:komikkurida/views/detail.dart';
-//import 'package:komikkurida/views/home.dart';
 import 'package:komikkurida/views/manga_list.dart';
 
 void main() => runApp(new MyApp());
@@ -11,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'GeekLib',
+      title: 'mangaX',
       debugShowCheckedModeBanner: false,
       theme: new ThemeData(
         primarySwatch: Colors.blue,
@@ -19,19 +16,6 @@ class MyApp extends StatelessWidget {
         platform: TargetPlatform.iOS,
       ),
       home: MangaScreen(),
-      onGenerateRoute: (settings) => generateRoute(settings),
-    );
-  }
-
-  ///Generate parameterized route --> e.g: detail/some book title
-  generateRoute(RouteSettings settings) {
-    final path = settings.name.split('/');
-    final title = path[1];
-
-    Book book = books.firstWhere((it) => it.title == title);
-    return MaterialPageRoute(
-      settings: settings,
-      builder: (context) => Detail(book),
     );
   }
 }
